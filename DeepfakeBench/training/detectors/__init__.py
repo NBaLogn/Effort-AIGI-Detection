90f814571f6faf8a3a772f111d798bfdf81389ab
Logan Nguyen <36346192+NBaLogn@users.noreply.github.com>
2025-12-03 11:53:05 +0700

    Fix import error in ProDet detector and CUDA check in test.py, add missing bi_online_generation.py

--- DeepfakeBench/training/detectors/__init__.py
+++ DeepfakeBench/training/detectors/__init__.py
@@ -8,5 +8,4 @@ sys.path.append(project_root_dir)
 
 from utils.registry import DETECTOR
 from .effort_detector import EffortDetector
-from .huggingface_detector import HuggingFaceCLIPDetector
-from .prodet_detector import ProDetDetector
+from .ProDet_detector import ProDetDetector
