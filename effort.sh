021feea4f269644387db39eeddb2fe87af203a8b
Logan Nguyen <36346192+NBaLogn@users.noreply.github.com>
2025-12-19 16:49:55 +0700

    fix: Corrected dataset paths in the evaluation script and updated evaluation results and logs.

--- effort.sh
+++ effort.sh
@@ -39,12 +39,12 @@ uv run DeepfakeBench/training/evaluate_finetune.py \
     --detector_config DeepfakeBench/training/config/detector/effort_finetune.yaml \
     --weights "DeepfakeBench/training/logs/batchedFaces2000/test/combined_test/ckpt_best.pth" \
     --test_dataset "/Volumes/Crucial/Large_Downloads/AI/DATASETS/SAMPLED/df40/CollabDiff" \
-    "/Volumes/Crucial/Large_Downloads/SAMPLED/df40/heygen_new" \
-    "/Volumes/Crucial/Large_Downloads/SAMPLED/df40/MidJourney" \
-    "/Volumes/Crucial/Large_Downloads/SAMPLED/df40/stargan" \
-    "/Volumes/Crucial/Large_Downloads/SAMPLED/df40/starganv2" \
-    "/Volumes/Crucial/Large_Downloads/SAMPLED/df40/styleclip" \
-    "/Volumes/Crucial/Large_Downloads/SAMPLED/df40/whichfaceisreal" \
+    "/Volumes/Crucial/Large_Downloads/AI/DATASETS/SAMPLED/df40/heygen_new" \
+    "/Volumes/Crucial/Large_Downloads/AI/DATASETS/SAMPLED/df40/MidJourney" \
+    "/Volumes/Crucial/Large_Downloads/AI/DATASETS/SAMPLED/df40/stargan" \
+    "/Volumes/Crucial/Large_Downloads/AI/DATASETS/SAMPLED/df40/starganv2" \
+    "/Volumes/Crucial/Large_Downloads/AI/DATASETS/SAMPLED/df40/styleclip" \
+    "/Volumes/Crucial/Large_Downloads/AI/DATASETS/SAMPLED/df40/whichfaceisreal" \
     --output_dir evaluation_results
 
 # =============================================
