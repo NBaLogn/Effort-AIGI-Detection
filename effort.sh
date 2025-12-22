9bdc2e210828ffc66e41446711b168d747975cc8
Logan Nguyen <36346192+NBaLogn@users.noreply.github.com>
2025-12-22 09:35:59 +0700

    chore: Update evaluation results, remove old logs and evaluation files, and add Node.js package files for training.

--- effort.sh
+++ effort.sh
@@ -10,7 +10,7 @@
 #     --test_dataset UADFV
 
 # =============================================
-# 2. FINE-TUNING ON UADFV DATASET
+# 2. FINE-TUNING 
 # =============================================
 # uv run DeepfakeBench/training/finetune.py \
 #     --detector_config DeepfakeBench/training/config/detector/effort_finetune.yaml \
@@ -20,32 +20,33 @@
 
 #use direct image loading instead of the processed images
 #use direct paths
-# uv run '/Users/logan/Developer/WORK/DEEPFAKE_DETECTION/Effort-AIGI-Detection/DeepfakeBench/training/finetune.py' \
-#     --detector_config '/Users/logan/Developer/WORK/DEEPFAKE_DETECTION/Effort-AIGI-Detection/DeepfakeBench/training/config/detector/effort_finetune.yaml' \
-#     --train_dataset '/Volumes/Crucial/Large_Downloads/AI/DATASETS/SAMPLED/Chameleon_retinafaces_2000/train' \
-#     '/Volumes/Crucial/Large_Downloads/AI/DATASETS/SAMPLED/GenImageFaces_sampled_structured_2000/train' \
-#     '/Volumes/Crucial/Large_Downloads/AI/DATASETS/SAMPLED/quan_dataset_2000/train' \
-#     '/Volumes/Crucial/Large_Downloads/AI/DATASETS/SAMPLED/quan_faceswap2000/train' \
-#     --test_dataset '/Volumes/Crucial/Large_Downloads/AI/DATASETS/SAMPLED/Chameleon_retinafaces_2000/val' \
-#     '/Volumes/Crucial/Large_Downloads/AI/DATASETS/SAMPLED/GenImageFaces_sampled_structured_2000/val' \
-#     '/Volumes/Crucial/Large_Downloads/AI/DATASETS/SAMPLED/quan_dataset_2000/val' \
-#     '/Volumes/Crucial/Large_Downloads/AI/DATASETS/SAMPLED/quan_faceswap2000/val' \
-#     --pretrained_weights '/Volumes/Crucial/Large_Downloads/AI/WEIGHTS/effort/effort_clip_L14_trainOn_FaceForensic.pth'
+uv run '/Users/logan/Developer/WORK/DEEPFAKE_DETECTION/Effort-AIGI-Detection/DeepfakeBench/training/finetune.py' \
+    --detector_config '/Users/logan/Developer/WORK/DEEPFAKE_DETECTION/Effort-AIGI-Detection/DeepfakeBench/training/config/detector/effort_finetune.yaml' \
+    --train_dataset '/Volumes/Crucial/Large_Downloads/AI/DATASETS/SAMPLED/Chameleon_retinafaces/train' \
+    '/Volumes/Crucial/Large_Downloads/AI/DATASETS/SAMPLED/GenImageFaces_2ndpass/train' \
+    '/Volumes/Crucial/Large_Downloads/AI/DATASETS/SAMPLED/quan_faceswap2000/train' \
+    '/Volumes/Crucial/Large_Downloads/AI/DATASETS/quan_dataset/train' \
+    --test_dataset '/Volumes/Crucial/Large_Downloads/AI/DATASETS/SAMPLED/Chameleon_retinafaces/val' \
+    '/Volumes/Crucial/Large_Downloads/AI/DATASETS/SAMPLED/GenImageFaces_2ndpass/val' \
+    '/Volumes/Crucial/Large_Downloads/AI/DATASETS/SAMPLED/quan_faceswap2000/val' \
+    '/Volumes/Crucial/Large_Downloads/AI/DATASETS/quan_dataset/val' \
+    --pretrained_weights '/Volumes/Crucial/Large_Downloads/AI/WEIGHTS/effort/effort_clip_L14_trainOn_FaceForensic.pth'
 
 # =============================================
 # 3. EVALUATION OF FINE-TUNED MODEL
 # =============================================
-uv run DeepfakeBench/training/evaluate_finetune.py \
-    --detector_config DeepfakeBench/training/config/detector/effort_finetune.yaml \
-    --weights "DeepfakeBench/training/logs/batchedFaces2000/test/combined_test/ckpt_best.pth" \
-    --test_dataset "/Volumes/Crucial/Large_Downloads/AI/DATASETS/SAMPLED/df40/CollabDiff" \
-    "/Volumes/Crucial/Large_Downloads/AI/DATASETS/SAMPLED/df40/heygen_new" \
-    "/Volumes/Crucial/Large_Downloads/AI/DATASETS/SAMPLED/df40/MidJourney" \
-    "/Volumes/Crucial/Large_Downloads/AI/DATASETS/SAMPLED/df40/stargan" \
-    "/Volumes/Crucial/Large_Downloads/AI/DATASETS/SAMPLED/df40/starganv2" \
-    "/Volumes/Crucial/Large_Downloads/AI/DATASETS/SAMPLED/df40/styleclip" \
-    "/Volumes/Crucial/Large_Downloads/AI/DATASETS/SAMPLED/df40/whichfaceisreal" \
-    --output_dir evaluation_results
+# uv run DeepfakeBench/training/evaluate_finetune.py \
+#     --detector_config DeepfakeBench/training/config/detector/effort_finetune.yaml \
+#     --weights "DeepfakeBench/training/logs/batchFaces2000/test/combined_test/batchFaces2000.pth" \
+#     --test_dataset "/Volumes/Crucial/Large_Downloads/AI/DATASETS/SAMPLED/Chameleon_retinafaces/val" \
+#     --output_dir evaluation_results
+    
+    # "/Volumes/Crucial/Large_Downloads/AI/DATASETS/SAMPLED/df40/heygen_new" \
+    # "/Volumes/Crucial/Large_Downloads/AI/DATASETS/SAMPLED/df40/MidJourney" \
+    # "/Volumes/Crucial/Large_Downloads/AI/DATASETS/SAMPLED/df40/stargan" \
+    # "/Volumes/Crucial/Large_Downloads/AI/DATASETS/SAMPLED/df40/starganv2" \
+    # "/Volumes/Crucial/Large_Downloads/AI/DATASETS/SAMPLED/df40/styleclip" \
+    # "/Volumes/Crucial/Large_Downloads/AI/DATASETS/SAMPLED/df40/whichfaceisreal" \
 
 # =============================================
 # 4. INFERENCE WITH FINE-TUNED MODEL
