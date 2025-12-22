1347e849c16473dfc3f08cff1302a7cfdb66fa0a
Logan Nguyen <36346192+NBaLogn@users.noreply.github.com>
2025-12-22 14:09:17 +0700

    misc

--- infer.sh
+++ infer.sh
@@ -3,7 +3,7 @@
 # =============================================
 
 uv run DeepfakeBench/training/inference.py \
-    --detector_config DeepfakeBench/training/config/detector/effort_finetune.yaml \
-    --landmark_model DeepfakeBench/preprocessing/shape_predictor_81_face_landmarks.dat \
+    --detector_config 'DeepfakeBench/training/config/detector/effort_finetune.yaml' \
+    --landmark_model  'DeepfakeBench/preprocessing/shape_predictor_81_face_landmarks.dat' \
     --weights 'DeepfakeBench/training/logs/batchFaces2000/test/combined_test/batchFaces2000.pth' \
-    --image '/Volumes/Crucial/Large_Downloads/AI/DATASETS/SAMPLED/quan_dataset_2000/val'
+    --image   'DeepfakeBench/facedata/quan_faceswap2000/val'
