68f5fecd36db79ed0f6aa86d3d0dc5acb7ea6bdf
Logan Nguyen <36346192+NBaLogn@users.noreply.github.com>
2025-12-26 16:31:18 +0700

    misc

--- eval.sh
+++ eval.sh
@@ -6,7 +6,7 @@ uv run 'DeepfakeBench/training/evaluate_finetune.py' \
     --detector_config \
         'DeepfakeBench/training/config/detector/effort_finetune.yaml' \
     --weights \
-        'DeepfakeBench/training/logs/batchFacesAll/test/combined_test/ckpt_best.pth' \
+        'DeepfakeBench/training/logs/batchFacesAll/test/combined_test/batchFacesAll.pth' \
     --test_dataset \
         'DeepfakeBench/facedata/quan_faceswap2000/val' \
         'DeepfakeBench/facedata/Chameleon_retinafaces/val' \
