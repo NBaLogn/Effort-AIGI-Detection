1bb0cc1d533c47c5ab8a95274d47cf0853073e09
Logan Nguyen <36346192+NBaLogn@users.noreply.github.com>
2026-01-16 13:34:29 +0700

    done

--- eval.sh
+++ eval.sh
@@ -8,16 +8,23 @@ uv run 'DeepfakeBench/training/evaluate_finetune.py' \
 	--weights \
 		'DeepfakeBench/training/weights/finetuned/newBatchFaces.pth' \
 	--test_dataset \
-		'DeepfakeBench/training/facedata/df40/CollabDiff' \
-        'DeepfakeBench/training/facedata/df40/deepfacelab' \
-        'DeepfakeBench/training/facedata/df40/heygen_new' \
-        'DeepfakeBench/training/facedata/df40/MidJourney' \
-        'DeepfakeBench/training/facedata/df40/stargan' \
-        'DeepfakeBench/training/facedata/df40/starganv2' \
-        'DeepfakeBench/training/facedata/df40/styleclip' \
-        'DeepfakeBench/training/facedata/df40/whichfaceisreal' \
+		'DeepfakeBench/training/facedata/TalkingHead_processed/LivePortrait' \
+		'DeepfakeBench/training/facedata/TalkingHead_processed/Hallo2' \
+		'DeepfakeBench/training/facedata/TalkingHead_processed/Hallo' \
+		'DeepfakeBench/training/facedata/TalkingHead_processed/EmoPortrait' \
+		'DeepfakeBench/training/facedata/TalkingHead_processed/AniPortraitVideo' \
+		'DeepfakeBench/training/facedata/TalkingHead_processed/AniPortraitAudio' \
 	--output_dir 'evaluation_results'
-		# 'DeepfakeBench/training/facedata/df40' \
+		# '/Volumes/Crucial/AI/DATASETS/TalkingHead_processed_additional_dataset_optimized/Hallo3' \
+		# '/Volumes/Crucial/AI/DATASETS/TalkingHead_processed_additional_dataset_optimized/MAGI-1' \
+		# 'DeepfakeBench/training/facedata/df40/CollabDiff' \
+        # 'DeepfakeBench/training/facedata/df40/deepfacelab' \
+        # 'DeepfakeBench/training/facedata/df40/heygen_new' \
+        # 'DeepfakeBench/training/facedata/df40/MidJourney' \
+        # 'DeepfakeBench/training/facedata/df40/stargan' \
+        # 'DeepfakeBench/training/facedata/df40/starganv2' \
+        # 'DeepfakeBench/training/facedata/df40/styleclip' \
+        # 'DeepfakeBench/training/facedata/df40/whichfaceisreal' \
 		# 'DeepfakeBench/training/facedata/Chameleon_retinafaces' \
 		# 'DeepfakeBench/training/facedata/GenImage_faces_09/ADM' \
 		# 'DeepfakeBench/training/facedata/GenImage_faces_09/BigGAN' \
