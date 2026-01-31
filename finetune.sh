b23504bef29c23fdc1ea8f257c098293348ef2c4
Logan Nguyen <36346192+NBaLogn@users.noreply.github.com>
2026-01-31 23:19:43 +0700

    correct train and test dataset spread

--- finetune.sh
+++ finetune.sh
@@ -10,6 +10,7 @@ uv run DeepfakeBench/training/finetune.py \
     --detector_config \
         DeepfakeBench/training/config/detector/effort_finetune.yaml \
     --train_dataset \
+        DeepfakeBench/training/facedata/Celeb-DF-v3_even_cropped/TalkingFace_EchoMimic \
         DeepfakeBench/training/facedata/Celeb-DF-v3_even_cropped/FaceReenact_DaGAN \
         DeepfakeBench/training/facedata/Celeb-DF-v3_even_cropped/FaceReenact_FSRT \
         DeepfakeBench/training/facedata/Celeb-DF-v3_even_cropped/FaceReenact_LIA \
@@ -32,53 +33,11 @@ uv run DeepfakeBench/training/finetune.py \
         \
         DeepfakeBench/training/facedata/deepfake_in_the_wild_22k_even_cropped \
         \
+        DeepfakeBench/training/facedata/df40_even_cropped/deepfacelab \
+        DeepfakeBench/training/facedata/df40_even_cropped/heygen_new \
+        \
         DeepfakeBench/training/facedata/DiFF_30k_even_cropped/I2I_DreamBooth \
         DeepfakeBench/training/facedata/DiFF_30k_even_cropped/T2I_HPS \
-        \
-        DeepfakeBench/training/facedata/DiffFace_even_cropped/ADM \
-        DeepfakeBench/training/facedata/DiffFace_even_cropped/DDPM \
-        DeepfakeBench/training/facedata/DiffFace_even_cropped/DiffSwap \
-        DeepfakeBench/training/facedata/DiffFace_even_cropped/LDM \
-        DeepfakeBench/training/facedata/DiffFace_even_cropped/Wild \
-        \
-        DeepfakeBench/training/facedata/humans100k_even_cropped \
-        \
-        DeepfakeBench/training/facedata/quan_faceswap2000_even_cropped/ \
-        \
-        DeepfakeBench/training/facedata/U_TalkingHead_cropped/AniPortraitAudio \
-        DeepfakeBench/training/facedata/U_TalkingHead_cropped/AniPortraitVideo \
-        DeepfakeBench/training/facedata/U_TalkingHead_cropped/EmoPortrait \
-        DeepfakeBench/training/facedata/U_TalkingHead_cropped/Hallo \
-        DeepfakeBench/training/facedata/U_TalkingHead_cropped/Hallo2 \
-        DeepfakeBench/training/facedata/U_TalkingHead_cropped/LivePortrait \
-        \
-        DeepfakeBench/training/facedata/df40_even_cropped/CollabDiff \
-        DeepfakeBench/training/facedata/df40_even_cropped/MidJourney \
-        DeepfakeBench/training/facedata/df40_even_cropped/heygen_new \
-        DeepfakeBench/training/facedata/df40_even_cropped/whichfaceisreal \
-    --test_dataset \
-        DeepfakeBench/training/facedata/Celeb-DF-v3_even_cropped/FaceReenact_HyperReenact \
-        DeepfakeBench/training/facedata/Celeb-DF-v3_even_cropped/TalkingFace_EchoMimic \
-        DeepfakeBench/training/facedata/Celeb-DF-v3_even_cropped/TalkingFace_EDTalk \
-        DeepfakeBench/training/facedata/Celeb-DF-v3_even_cropped/TalkingFace_FLOAT \
-        DeepfakeBench/training/facedata/Celeb-DF-v3_even_cropped/TalkingFace_Real3DPortrait \
-        \
-        DeepfakeBench/training/facedata/df40_even_cropped/deepfacelab \
-        DeepfakeBench/training/facedata/df40_even_cropped/stargan \
-        DeepfakeBench/training/facedata/df40_even_cropped/starganv2 \
-        DeepfakeBench/training/facedata/df40_even_cropped/styleclip \
-        \
-        DeepfakeBench/training/facedata/DiFF_30k_even_cropped/FE_CoDiff \
-        DeepfakeBench/training/facedata/DiFF_30k_even_cropped/FE_cycle_diff \
-        DeepfakeBench/training/facedata/DiFF_30k_even_cropped/FE_Imagic \
-        DeepfakeBench/training/facedata/DiFF_30k_even_cropped/FS_DCFace \
-        DeepfakeBench/training/facedata/DiFF_30k_even_cropped/FS_DiffFace \
-        DeepfakeBench/training/facedata/DiFF_30k_even_cropped/I2I_FreeDoM_I \
-        DeepfakeBench/training/facedata/DiFF_30k_even_cropped/I2I_LoRA \
-        DeepfakeBench/training/facedata/DiFF_30k_even_cropped/I2I_SDXL_Refine \
-        DeepfakeBench/training/facedata/DiFF_30k_even_cropped/T2I_FreeDoM_T \
-        DeepfakeBench/training/facedata/DiFF_30k_even_cropped/T2I_Midjourney \
-        DeepfakeBench/training/facedata/DiFF_30k_even_cropped/T2I_SDXL \
         \
         DeepfakeBench/training/facedata/DiffFace_even_cropped/DDIM \
         DeepfakeBench/training/facedata/DiffFace_even_cropped/Inpaint \
@@ -95,6 +54,48 @@ uv run DeepfakeBench/training/finetune.py \
         DeepfakeBench/training/facedata/DiffFace_even_cropped/stable_diffusion_v_2_1_text2img_p0g5 \
         DeepfakeBench/training/facedata/DiffFace_even_cropped/stable_diffusion_v_2_1_text2img_p1g7 \
         DeepfakeBench/training/facedata/DiffFace_even_cropped/stable_diffusion_v_2_1_text2img_p2g3 \
+        \
+        DeepfakeBench/training/facedata/humans100k_even_cropped \
+        \
+        DeepfakeBench/training/facedata/quan_faceswap2000_even_cropped/ \
+        \
+        DeepfakeBench/training/facedata/U_TalkingHead_cropped/AniPortraitAudio \
+        DeepfakeBench/training/facedata/U_TalkingHead_cropped/AniPortraitVideo \
+        DeepfakeBench/training/facedata/U_TalkingHead_cropped/EmoPortrait \
+        DeepfakeBench/training/facedata/U_TalkingHead_cropped/Hallo \
+        DeepfakeBench/training/facedata/U_TalkingHead_cropped/Hallo2 \
+        DeepfakeBench/training/facedata/U_TalkingHead_cropped/LivePortrait \
+        \
+    --test_dataset \
+        DeepfakeBench/training/facedata/Celeb-DF-v3_even_cropped/FaceReenact_HyperReenact \
+        DeepfakeBench/training/facedata/Celeb-DF-v3_even_cropped/TalkingFace_EDTalk \
+        DeepfakeBench/training/facedata/Celeb-DF-v3_even_cropped/TalkingFace_FLOAT \
+        DeepfakeBench/training/facedata/Celeb-DF-v3_even_cropped/TalkingFace_Real3DPortrait \
+        \
+        DeepfakeBench/training/facedata/df40_even_cropped/CollabDiff \
+        DeepfakeBench/training/facedata/df40_even_cropped/stargan \
+        DeepfakeBench/training/facedata/df40_even_cropped/starganv2 \
+        DeepfakeBench/training/facedata/df40_even_cropped/styleclip \
+        DeepfakeBench/training/facedata/df40_even_cropped/MidJourney \
+        DeepfakeBench/training/facedata/df40_even_cropped/whichfaceisreal \
+        \
+        DeepfakeBench/training/facedata/DiFF_30k_even_cropped/FE_CoDiff \
+        DeepfakeBench/training/facedata/DiFF_30k_even_cropped/FE_cycle_diff \
+        DeepfakeBench/training/facedata/DiFF_30k_even_cropped/FE_Imagic \
+        DeepfakeBench/training/facedata/DiFF_30k_even_cropped/FS_DCFace \
+        DeepfakeBench/training/facedata/DiFF_30k_even_cropped/FS_DiffFace \
+        DeepfakeBench/training/facedata/DiFF_30k_even_cropped/I2I_FreeDoM_I \
+        DeepfakeBench/training/facedata/DiFF_30k_even_cropped/I2I_LoRA \
+        DeepfakeBench/training/facedata/DiFF_30k_even_cropped/I2I_SDXL_Refine \
+        DeepfakeBench/training/facedata/DiFF_30k_even_cropped/T2I_FreeDoM_T \
+        DeepfakeBench/training/facedata/DiFF_30k_even_cropped/T2I_Midjourney \
+        DeepfakeBench/training/facedata/DiFF_30k_even_cropped/T2I_SDXL \
+        \
+        DeepfakeBench/training/facedata/DiffFace_even_cropped/ADM \
+        DeepfakeBench/training/facedata/DiffFace_even_cropped/DDPM \
+        DeepfakeBench/training/facedata/DiffFace_even_cropped/DiffSwap \
+        DeepfakeBench/training/facedata/DiffFace_even_cropped/LDM \
+        DeepfakeBench/training/facedata/DiffFace_even_cropped/Wild \
         \
         DeepfakeBench/training/facedata/quan_dataset_even_cropped/ \
     --pretrained_weights \
